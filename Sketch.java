e6edfaca048dcea42dd85850dee17d179b84d2f0
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-01-10 18:07:56 -0500

    5 learning objectives

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,15 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+  // flower variables
+  public float petalSize = random(5, 25);
+  public float petalDistance = petalSize / 1.5f;
+
+  // cloud variables
+  public int cloudX = 100;
+  public int cloudY = 100;
+  public int cloudW = 25;
+  public int cloudH = 10;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -16,21 +24,90 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(23, 184, 20);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    if (keyPressed){
+      // change background when key pressed
+      if(key == 'r'){
+        background (156, 20, 20);
+      }
+      else if(key == 'g'){
+        background (3, 255, 3);
+      }
+      else if(key == 'b'){
+        background(2, 149, 240);
+      }      
+    } 
+    
+    // draw cloud
+    stroke(0, 0, 0);
+    fill(255, 255, 255);
+    if (keyPressed){
+      if (keyCode == UP){
+        cloudY--;
+      }
+      else if (keyCode == DOWN){
+        cloudY++;
+      }
+      else if (keyCode == LEFT){
+        cloudX--;
+      }
+      else if(keyCode == RIGHT){
+        cloudX++;
+      }
+    }
+    ellipse(cloudX+cloudW, cloudY, cloudW, cloudH);
+    ellipse(cloudX+(cloudW/2), cloudY-cloudH, cloudW, cloudH);
+    ellipse(cloudX, cloudY, cloudW, cloudH);  
   }
+ 
+  public void mouseClicked(){
+    // make flower everytime the mouse is clicked
+    stroke(0, 0, 0);
+    fill(255, 255, 255);
+    ellipse (mouseX + petalDistance, mouseY, petalSize, petalSize);  
+    ellipse (mouseX - petalDistance, mouseY, petalSize, petalSize);  
+    ellipse(mouseX, mouseY + petalDistance, petalSize, petalSize);
+    ellipse(mouseX, mouseY - petalDistance, petalSize,petalSize);
+
+    // yellow center of flower
+    fill(255, 241, 51);
+    ellipse (mouseX, mouseY, petalSize, petalSize);
+}
+
+  public void mouseDragged(){
+    // make grass lines 
+    stroke(1, 71, 20);
+    fill(1, 71, 20);
+    ellipse(mouseX, mouseY, 1, 10);
+  }
+
+  public void mouseWheel(){
+    // draw tree
+    stroke(61, 41, 0);
+    fill(61, 41, 0);
+    rect(mouseX, mouseY, 5, 40);
+    
+    stroke(1, 71, 20);
+    fill(1, 71, 20);
+    ellipse(mouseX, mouseY, 25, 25);
+
+    stroke(1, 71, 20);
+    fill(1, 71, 20);
+    ellipse(mouseX - 7, mouseY +5, 25, 25);
+
+    stroke(1, 71, 20);
+    fill(1, 71, 20);
+    ellipse(mouseX + 10, mouseY +5, 25, 25);
+  }
+
+
   
   // define other methods down here.
 }
